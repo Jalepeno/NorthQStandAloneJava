aaff7d574af88cdbd6b07246bd144026b2f48f15
mandersen21 <mikkelandersen21@gmail.com>
2017-09-12 10:46:36 +0200

    Added controller, models and view

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import javax.swing.SwingUtilities;
+
+import Controller.QPlugController;
+import Models.QPlugModel;
+import View.GUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+            	
+            	GUI view = new GUI();
+                
+            	QPlugModel qPlugModel = new QPlugModel();
+                
+                QPlugController qPlugController = new QPlugController(view,qPlugModel);
+                
+                view.setVisible(true);
+            	
+            	
+            	
+//                GUI app = new GUI();
+//                app.setVisible(true);
+            }
+        });
+	}
+
+}
