943b51a0882e5db848aa44ecfd758f9f93df5144
mandersen21 <mikkelandersen21@gmail.com>
2017-09-12 10:46:36 +0200

    Added controller, models and view

---
+++ src/Controller/QPlugController.java
@@ -0,0 +1,12 @@
+package Controller;
+
+import Models.QPlugModel;
+import View.GUI;
+
+public class QPlugController {
+		
+	public QPlugController(GUI view, QPlugModel qPlugModel) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
