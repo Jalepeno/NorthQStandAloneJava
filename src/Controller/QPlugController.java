a4094297034d2dccca946fe76811227feacf5dde
Philip1992 <philler10@hotmail.com>
2017-09-15 09:45:12 +0200

    Revert "Added QPlugController models"
    This reverts commit d9432f9c19864c21a8c7f4e3ee6ff692dcb4ab6e.

--- src/Controller/QPlugController.java
+++ src/Controller/QPlugController.java
@@ -4,21 +4,9 @@ import Models.QPlugModel;
 import View.GUI;
 
 public class QPlugController {
-	QPlugModel qPlugModel;	
+		
 	public QPlugController(GUI view, QPlugModel qPlugModel) {
 		// TODO Auto-generated constructor stub
-	}
-	
-	public void turnOn(){
-		qPlugModel.setStatus(1);
-	}
-	
-	public void turnOff(){
-		qPlugModel.setStatus(0);
-	}
-	
-	public int getStatus(){
-		return qPlugModel.getStatus(); 
 	}
 
 }
--- src/Models/QPlugModel.java
+++ src/Models/QPlugModel.java
@@ -1,28 +1,5 @@
 package Models;
 
-
-
 public class QPlugModel {
-	
-	int status;
-	
-	public QPlugModel(){
-		
-	}
-	
-	public QPlugModel(int status){
-		this.status = status;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
-	
 
 }
-
-
