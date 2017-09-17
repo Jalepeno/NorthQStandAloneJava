e005a0eb7fb1a3c97756ae32b5d8ff7fb662f20f
mandersen21 <mikkelandersen21@gmail.com>
2017-09-17 14:12:20 +0200

    Added app code.

--- src/main/java/com/northqstandalone/maven/app/App.java
+++ src/main/java/com/northqstandalone/maven/app/App.java
@@ -1,9 +1,26 @@
 package com.northqstandalone.maven.app;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello all, Maven works" );
-    }
+import com.northqstandalone.maven.controllers.QMotionController;
+import com.northqstandalone.maven.controllers.QPlugController;
+import com.northqstandalone.maven.models.QMotionModel;
+import com.northqstandalone.maven.models.QPlugModel;
+import com.northqstandalone.maven.view.View;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello all, Maven works");
+
+		// View
+		View view = new View();
+
+		// QPlug
+		QPlugModel qPlugModel = new QPlugModel();
+		QPlugController qPlugController = new QPlugController(view, qPlugModel);
+
+		// QMotion
+		QMotionModel qMotionModel = new QMotionModel();
+		QMotionController qMotionController = new QMotionController(view, qMotionModel);
+		
+		view.setVisible(true);
+	}
 }
