f8aff4ceabcf6d0580e22000ed5805acffd3897e
mandersen21 <mikkelandersen21@gmail.com>
2017-09-17 10:00:32 +0200

    delete old project.

--- src/Services/IQPlugService.java
+++
@@ -1,5 +0,0 @@
-package Services;
-
-public interface IQPlugService {
-	// TODO: Implement interface
-}
--- src/Services/ITokenService.java
+++
@@ -1,5 +0,0 @@
-package Services;
-
-public interface ITokenService {
-	// TODO: Implement interface
-}
