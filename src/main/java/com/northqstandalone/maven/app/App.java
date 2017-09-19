c3ab19f3410016818de1fdec92de621c2a79bbdc
DarkToucan <danmail@mail.dk>
2017-09-19 16:31:33 +0200

    Signed-off-by: DarkToucan <danmail@mail.dk>

--- src/main/java/com/northqstandalone/maven/app/App.java
+++ src/main/java/com/northqstandalone/maven/app/App.java
@@ -7,19 +7,33 @@ import com.northqstandalone.maven.models.QPlugModel;
 import com.northqstandalone.maven.view.View;
 
 public class App {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// View
-		View view = new View();
+        // View
+        View view = new View();
 
-		// QPlug
-		QPlugModel qPlugModel = new QPlugModel();
-		QPlugController qPlugController = new QPlugController(view, qPlugModel);
+        // QPlug
+        QPlugModel qPlugModel = new QPlugModel();
+        QPlugController qPlugController = new QPlugController(view, qPlugModel);
 
-		// QMotion
-		QMotionModel qMotionModel = new QMotionModel();
-		QMotionController qMotionController = new QMotionController(view, qMotionModel);
-		
-		view.setVisible(true);
-	}
+        // QMotion
+        QMotionModel qMotionModel = new QMotionModel();
+        QMotionController qMotionController = new QMotionController(view, qMotionModel);
+
+        view.setVisible(true);
+
+        /*
+         * Test code
+         * NorthQRestfulUtils service = new NorthQRestfulUtils();
+         * String token = service.getToken();
+         * service.getGetawayStatus("0000003652", "2166", token);
+         *
+         * QMotionService motion = new QMotionService();
+         * System.out.println(motion.armMotion(token).readEntity(String.class).toString());
+         * System.out.println(service.getNotifications("2166", token, "1828", "1").readEntity(String.class).toString());
+         * motion.updateQMotionStatus(0, token);
+         * System.out.println(motion.disarmMotion(token).readEntity(String.class).toString());
+         */
+
+    }
 }
