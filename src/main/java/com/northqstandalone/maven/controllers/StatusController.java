e4636bf5090621a76da6969161445ac8d5e133fd
mandersen21 <mikkelandersen21@gmail.com>
2017-09-22 10:18:45 +0200

    added status for qplug

--- src/main/java/com/northqstandalone/maven/controllers/StatusController.java
+++ src/main/java/com/northqstandalone/maven/controllers/StatusController.java
@@ -1,5 +1,8 @@
 package com.northqstandalone.maven.controllers;
 
+import java.util.Map;
+
+import com.google.gson.Gson;
 import com.northqstandalone.maven.services.NorthQRestfulUtils;
 
 public class StatusController {
@@ -10,12 +13,16 @@ public class StatusController {
 		this.utils = utils;
 	}
 	
-	public boolean getQPlugStatus(String token) {
-		
+	public boolean getQPlugStatus(String token) {	
+		String plugStatus;
 		String gatewayStatus = utils.getGatewayStatusJSON("0000003652", "2166", token);
 		String status = utils.getJsonMap(gatewayStatus).get("BinarySwitches").toString();
-		
-		return status.contains("pos=255.0") ? true : false;
+		Gson gson = new Gson();
+		Object[] test = gson.fromJson(status, Object[].class);
+		Map<String, Object> json = utils.getJsonMap(test[0].toString());
+		plugStatus = json.get("pos").toString();
+		System.out.println(plugStatus);
+		return plugStatus.equals("255.0");
 	}
 
 }
