fb293f5d71a2b3f030b2eeed04632b658fd92a50
Jakob Skriver <Jakob_skiver@hotmail.com>
2017-09-22 14:49:33 +0200

    Added Initial Status check for QMotion button, and a labe that changes

--- src/main/java/com/northqstandalone/maven/controllers/StatusController.java
+++ src/main/java/com/northqstandalone/maven/controllers/StatusController.java
@@ -17,11 +17,26 @@ public class StatusController {
 		String plugStatus;
 		String gatewayStatus = utils.getGatewayStatusJSON("0000003652", "2166", token);
 		String status = utils.getJsonMap(gatewayStatus).get("BinarySwitches").toString();
+		
 		Gson gson = new Gson();
 		Object[] test = gson.fromJson(status, Object[].class);
 		Map<String, Object> json = utils.getJsonMap(test[0].toString());
 		plugStatus = json.get("pos").toString();
+		
 		return plugStatus.equals("255.0");
+	}
+	
+	public boolean getQMotionStatus(String token) {
+		String motionStatus;
+		String gatewayStatus = utils.getGatewayStatusJSON("0000003652", "2166", token);
+		String status = utils.getJsonMap(gatewayStatus).get("BinarySensors").toString();
+		
+		Gson gson = new Gson();
+		Object[] test = gson.fromJson(status, Object[].class);
+		Map<String, Object> json = utils.getJsonMap(test[0].toString());
+		motionStatus = json.get("armed").toString();
+		
+		return motionStatus.equals("1.0");
 	}
 
 }
